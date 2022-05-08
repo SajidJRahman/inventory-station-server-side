c331d6bf2a3e177e12c67c22c994abb0b00f5cab
Sajid Rahman <sajidjrahman@icloud.com>
2022-05-08 13:05:57 +0200

    finished server side

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
@@ -16,6 +17,7 @@ const run = async () => {
         await client.connect();
         const products = client.db("inventoryStation").collection("products");
 
+        // GET API
         app.get('/products', async (req, res) => {
             const query = {};
             const cursor = products.find(query);
@@ -23,12 +25,14 @@ const run = async () => {
             res.send(result);
         });
 
+        // POST API
         app.post('/products', async (req, res) => {
             const insertedProducts = req.body;
             const result = await products.insertOne(insertedProducts);
             res.send(result);
         });
 
+        // GET API For Single ID
         app.get('/products/:id', async (req, res) => {
             const productsId = req.params.id;
             const query = { _id: ObjectId(productsId) };
@@ -36,6 +40,7 @@ const run = async () => {
             res.send(result);
         });
 
+        // DELETE API
         app.delete('/products/:id', async (req, res) => {
             const productsId = req.params.id;
             const query = { _id: ObjectId(productsId) };
@@ -43,6 +48,7 @@ const run = async () => {
             res.send(result);
         });
 
+        // UPDATE, PATCH OR PUT API
         app.put('/products/:id', async (req, res) => {
             const productsId = req.params.id;
             const newQuantity = req.body;
