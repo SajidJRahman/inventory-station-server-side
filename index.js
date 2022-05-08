d341f1df9a70ad62f392a90c2c51e0153587176e
Sajid Rahman <sajidjrahman@icloud.com>
2022-05-08 18:17:15 +0200

    finished

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// DB URI
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.f7xnm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -25,13 +26,6 @@ const run = async () => {
             res.send(result);
         });
 
-        // POST API
-        app.post('/products', async (req, res) => {
-            const insertedProducts = req.body;
-            const result = await products.insertOne(insertedProducts);
-            res.send(result);
-        });
-
         // GET API For Single ID
         app.get('/products/:id', async (req, res) => {
             const productsId = req.params.id;
@@ -40,11 +34,26 @@ const run = async () => {
             res.send(result);
         });
 
-        // DELETE API
-        app.delete('/products/:id', async (req, res) => {
-            const productsId = req.params.id;
-            const query = { _id: ObjectId(productsId) };
-            const result = await products.deleteOne(query);
+        // GET API For My Items
+        app.get('/my-items', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const cursor = products.find(query);
+            const myAddedItems = await cursor.toArray();
+            res.send(myAddedItems);
+        });
+
+        // POST API
+        app.post('/products', async (req, res) => {
+            const insertedProducts = req.body;
+            const result = await products.insertOne(insertedProducts);
+            res.send(result);
+        });
+
+        // POST API For My Items
+        app.post('/my-items', async (req, res) => {
+            const insertedProducts = req.body;
+            const result = await products.insertOne(insertedProducts);
             res.send(result);
         });
 
@@ -62,6 +71,14 @@ const run = async () => {
             const result = await products.updateOne(query, updatedQuantity, options);
             res.send(result);
         });
+
+        // DELETE API
+        app.delete('/products/:id', async (req, res) => {
+            const productsId = req.params.id;
+            const query = { _id: ObjectId(productsId) };
+            const result = await products.deleteOne(query);
+            res.send(result);
+        });
     }
     finally {
 
@@ -69,11 +86,12 @@ const run = async () => {
 }
 run().catch(console.dir);
 
-
+// TEST Message
 app.get('/', (req, res) => {
     res.send('warehouse-management');
 });
 
+// PORT
 app.listen(port, () => {
     console.log('Listening to port', port);
 })
